d55b50f529e92a69a49b167adf798aa4b2257e06
fredericpoitevin <frederic.poitevin@gmail.com>
2024-12-10 15:01:18 +0000

    Deploying to gh-pages from @ lcls-mlcv/lcls-mlcv.github.io@896fb66c843b2ca01942a7d4f3c3e2af80732459 🚀

--- assets/img/jpegify.sh
+++ assets/img/jpegify.sh
@@ -1,1 +1,17 @@
-magick convert John_Winnicki_SC24.pdf -sampling-factor 4:2:0 -strip -quality 85 -interlace JPEG -colorspace RGB John_Winnicki_SC24.jpg
+#!/bin/bash
+
+input=$1
+output=$2
+
+echo "converting $input to $output"
+
+magick convert $input \
+-resize 400 \
+-sampling-factor 4:2:0 \
+-strip \
+-quality 85 \
+-interlace JPEG \
+$output
+
+echo "renaming $input to ${input}.bak"
+mv $input ${input}.bak
